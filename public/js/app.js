ba6c82f36d10072140e0555d30246a2b0a18592b
dammy <adiodamilola@yahoo.com>
2017-10-11 13:18:27 +0100

    Add form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,16 @@ socket.on("message", function(message) {
 	console.log("New message:");
 	console.log(message.text);
 });
+
+var form = document.getElementById("message-form"),
+	input = form.getElementsByTagName("input")[0];
+
+form.addEventListener("submit", function(e) {
+	e.preventDefault();
+
+	socket.emit("message", {
+		text: input.value
+	});
+
+	input.value = "";
+});
